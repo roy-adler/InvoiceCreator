6401407e8ef6978036b7776b58b7b18071ef7936
Roy Adler <royadler@outlook.com>
2020-12-02 19:23:14 +0100

    Add new invoice Creator for c#

---
+++ Invoice_Creator/InvoiceCreator.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace Invoice_Creator
+{
+		partial class InvoiceCreator
+		{
+				/// <summary>
+				/// Required designer variable.
+				/// </summary>
+				private System.ComponentModel.IContainer components = null;
+
+				/// <summary>
+				/// Clean up any resources being used.
+				/// </summary>
+				/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+				protected override void Dispose(bool disposing)
+				{
+						if (disposing && (components != null))
+						{
+								components.Dispose();
+						}
+						base.Dispose(disposing);
+				}
+
+				#region Windows Form Designer generated code
+
+				/// <summary>
+				/// Required method for Designer support - do not modify
+				/// the contents of this method with the code editor.
+				/// </summary>
+				private void InitializeComponent()
+				{
+						this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+						this.tbOrganization = new System.Windows.Forms.TextBox();
+						this.tbSurname = new System.Windows.Forms.TextBox();
+						this.tbName = new System.Windows.Forms.TextBox();
+						this.tbStreet = new System.Windows.Forms.TextBox();
+						this.tbZipCode = new System.Windows.Forms.TextBox();
+						this.tbState = new System.Windows.Forms.TextBox();
+						this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+						this.tbOrgOrganization = new System.Windows.Forms.TextBox();
+						this.tbOrgSurname = new System.Windows.Forms.TextBox();
+						this.tbOrgName = new System.Windows.Forms.TextBox();
+						this.tbOrgStreet = new System.Windows.Forms.TextBox();
+						this.tbOrgZipCode = new System.Windows.Forms.TextBox();
+						this.tbOrgState = new System.Windows.Forms.TextBox();
+						this.btnCreateInvoice = new System.Windows.Forms.Button();
+						this.comboBoxLatexTemplate = new System.Windows.Forms.ComboBox();
+						this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+						this.flowLayoutPanel1.SuspendLayout();
+						this.flowLayoutPanel2.SuspendLayout();
+						this.flowLayoutPanel3.SuspendLayout();
+						this.SuspendLayout();
+						// 
+						// flowLayoutPanel1
+						// 
+						this.flowLayoutPanel1.Controls.Add(this.tbOrganization);
+						this.flowLayoutPanel1.Controls.Add(this.tbSurname);
+						this.flowLayoutPanel1.Controls.Add(this.tbName);
+						this.flowLayoutPanel1.Controls.Add(this.tbStreet);
+						this.flowLayoutPanel1.Controls.Add(this.tbZipCode);
+						this.flowLayoutPanel1.Controls.Add(this.tbState);
+						this.flowLayoutPanel1.Location = new System.Drawing.Point(35, 46);
+						this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+						this.flowLayoutPanel1.Size = new System.Drawing.Size(147, 161);
+						this.flowLayoutPanel1.TabIndex = 0;
+						// 
+						// tbOrganization
+						// 
+						this.tbOrganization.Location = new System.Drawing.Point(3, 3);
+						this.tbOrganization.Name = "tbOrganization";
+						this.tbOrganization.Size = new System.Drawing.Size(140, 20);
+						this.tbOrganization.TabIndex = 3;
+						// 
+						// tbSurname
+						// 
+						this.tbSurname.Location = new System.Drawing.Point(3, 29);
+						this.tbSurname.Name = "tbSurname";
+						this.tbSurname.Size = new System.Drawing.Size(140, 20);
+						this.tbSurname.TabIndex = 4;
+						// 
+						// tbName
+						// 
+						this.tbName.Location = new System.Drawing.Point(3, 55);
+						this.tbName.Name = "tbName";
+						this.tbName.Size = new System.Drawing.Size(140, 20);
+						this.tbName.TabIndex = 5;
+						// 
+						// tbStreet
+						// 
+						this.tbStreet.Location = new System.Drawing.Point(3, 81);
+						this.tbStreet.Name = "tbStreet";
+						this.tbStreet.Size = new System.Drawing.Size(140, 20);
+						this.tbStreet.TabIndex = 6;
+						// 
+						// tbZipCode
+						// 
+						this.tbZipCode.Location = new System.Drawing.Point(3, 107);
+						this.tbZipCode.Name = "tbZipCode";
+						this.tbZipCode.Size = new System.Drawing.Size(140, 20);
+						this.tbZipCode.TabIndex = 7;
+						// 
+						// tbState
+						// 
+						this.tbState.Location = new System.Drawing.Point(3, 133);
+						this.tbState.Name = "tbState";
+						this.tbState.Size = new System.Drawing.Size(140, 20);
+						this.tbState.TabIndex = 8;
+						// 
+						// flowLayoutPanel2
+						// 
+						this.flowLayoutPanel2.Controls.Add(this.tbOrgOrganization);
+						this.flowLayoutPanel2.Controls.Add(this.tbOrgSurname);
+						this.flowLayoutPanel2.Controls.Add(this.tbOrgName);
+						this.flowLayoutPanel2.Controls.Add(this.tbOrgStreet);
+						this.flowLayoutPanel2.Controls.Add(this.tbOrgZipCode);
+						this.flowLayoutPanel2.Controls.Add(this.tbOrgState);
+						this.flowLayoutPanel2.Location = new System.Drawing.Point(266, 46);
+						this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+						this.flowLayoutPanel2.Size = new System.Drawing.Size(147, 161);
+						this.flowLayoutPanel2.TabIndex = 9;
+						// 
+						// tbOrgOrganization
+						// 
+						this.tbOrgOrganization.Location = new System.Drawing.Point(3, 3);
+						this.tbOrgOrganization.Name = "tbOrgOrganization";
+						this.tbOrgOrganization.Size = new System.Drawing.Size(140, 20);
+						this.tbOrgOrganization.TabIndex = 3;
+						// 
+						// tbOrgSurname
+						// 
+						this.tbOrgSurname.Location = new System.Drawing.Point(3, 29);
+						this.tbOrgSurname.Name = "tbOrgSurname";
+						this.tbOrgSurname.Size = new System.Drawing.Size(140, 20);
+						this.tbOrgSurname.TabIndex = 4;
+						// 
+						// tbOrgName
+						// 
+						this.tbOrgName.Location = new System.Drawing.Point(3, 55);
+						this.tbOrgName.Name = "tbOrgName";
+						this.tbOrgName.Size = new System.Drawing.Size(140, 20);
+						this.tbOrgName.TabIndex = 5;
+						// 
+						// tbOrgStreet
+						// 
+						this.tbOrgStreet.Location = new System.Drawing.Point(3, 81);
+						this.tbOrgStreet.Name = "tbOrgStreet";
+						this.tbOrgStreet.Size = new System.Drawing.Size(140, 20);
+						this.tbOrgStreet.TabIndex = 6;
+						// 
+						// tbOrgZipCode
+						// 
+						this.tbOrgZipCode.Location = new System.Drawing.Point(3, 107);
+						this.tbOrgZipCode.Name = "tbOrgZipCode";
+						this.tbOrgZipCode.Size = new System.Drawing.Size(140, 20);
+						this.tbOrgZipCode.TabIndex = 7;
+						// 
+						// tbOrgState
+						// 
+						this.tbOrgState.Location = new System.Drawing.Point(3, 133);
+						this.tbOrgState.Name = "tbOrgState";
+						this.tbOrgState.Size = new System.Drawing.Size(140, 20);
+						this.tbOrgState.TabIndex = 8;
+						// 
+						// btnCreateInvoice
+						// 
+						this.btnCreateInvoice.Location = new System.Drawing.Point(130, 3);
+						this.btnCreateInvoice.Name = "btnCreateInvoice";
+						this.btnCreateInvoice.Size = new System.Drawing.Size(112, 23);
+						this.btnCreateInvoice.TabIndex = 10;
+						this.btnCreateInvoice.Text = "Create Invoice";
+						this.btnCreateInvoice.UseVisualStyleBackColor = true;
+						// 
+						// comboBoxLatexTemplate
+						// 
+						this.comboBoxLatexTemplate.FormattingEnabled = true;
+						this.comboBoxLatexTemplate.Location = new System.Drawing.Point(3, 3);
+						this.comboBoxLatexTemplate.Name = "comboBoxLatexTemplate";
+						this.comboBoxLatexTemplate.Size = new System.Drawing.Size(121, 21);
+						this.comboBoxLatexTemplate.TabIndex = 11;
+						// 
+						// flowLayoutPanel3
+						// 
+						this.flowLayoutPanel3.Controls.Add(this.comboBoxLatexTemplate);
+						this.flowLayoutPanel3.Controls.Add(this.btnCreateInvoice);
+						this.flowLayoutPanel3.Location = new System.Drawing.Point(87, 225);
+						this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+						this.flowLayoutPanel3.Size = new System.Drawing.Size(252, 32);
+						this.flowLayoutPanel3.TabIndex = 12;
+						// 
+						// InvoiceCreator
+						// 
+						this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+						this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+						this.ClientSize = new System.Drawing.Size(445, 281);
+						this.Controls.Add(this.flowLayoutPanel3);
+						this.Controls.Add(this.flowLayoutPanel1);
+						this.Controls.Add(this.flowLayoutPanel2);
+						this.Name = "InvoiceCreator";
+						this.Text = "Invoice Creator";
+						this.flowLayoutPanel1.ResumeLayout(false);
+						this.flowLayoutPanel1.PerformLayout();
+						this.flowLayoutPanel2.ResumeLayout(false);
+						this.flowLayoutPanel2.PerformLayout();
+						this.flowLayoutPanel3.ResumeLayout(false);
+						this.ResumeLayout(false);
+
+				}
+
+				#endregion
+
+				private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+				private System.Windows.Forms.TextBox tbOrganization;
+				private System.Windows.Forms.TextBox tbSurname;
+				private System.Windows.Forms.TextBox tbName;
+				private System.Windows.Forms.TextBox tbStreet;
+				private System.Windows.Forms.TextBox tbZipCode;
+				private System.Windows.Forms.TextBox tbState;
+				private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+				private System.Windows.Forms.TextBox tbOrgOrganization;
+				private System.Windows.Forms.TextBox tbOrgSurname;
+				private System.Windows.Forms.TextBox tbOrgName;
+				private System.Windows.Forms.TextBox tbOrgStreet;
+				private System.Windows.Forms.TextBox tbOrgZipCode;
+				private System.Windows.Forms.TextBox tbOrgState;
+				private System.Windows.Forms.Button btnCreateInvoice;
+				private System.Windows.Forms.ComboBox comboBoxLatexTemplate;
+				private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+		}
+}
+
